dfd4f20cd57c34d05a4ae2310a9efab2ff1e4418
albertocavia <acaviaz@moradas.es>
2015-07-08 19:08:39 +0200

    Quiz15

--- models/model.js
+++ models/model.js
@@ -31,8 +31,19 @@ var sequelize = new Sequelize (null,null,null,
 			);
 			
 // importar la definicion de la table Quiz en quiz.js
-var Quiz = sequelize.import (path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+// relaciones estre tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz =Quiz;  // exportar definicion de la tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
